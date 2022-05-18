7cbb3c9121aaaca4485bed67664196a0e2436578
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-05-18 08:55:02 +0530

    updated footer

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const _ = require("lodash");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const homeStartingContent ="Hey Guys & Girls, This is my First Blog Website. You can also make a new Post by going on :- localhost:4000/compose in the Search Bar...Have a Good Day!!";
+const homeStartingContent ="Hey Guys & Girls, This is my First Blog Website. You can also make a new Post by going on Compose Post";
 const aboutContent = "This is the version 2.0 of my Blog-Website, you can add your thoughts too in the form of a Blog. This will have more features in future... So, Till then enjoy this version...";
 const contactContent = "Mail id:- pryanshubharti9412@gmail.com";
 

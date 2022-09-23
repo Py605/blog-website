f4774231ec4d2bcca2b91b630f420d1c9dbdbeac
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-09-23 22:18:43 +0530

    updated

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ let posts = [];
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res){
   res.render('home',{
